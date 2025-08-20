e4c3e240e9fd73a189e6fe7706bd60556f0cd3ce
hsrokon <hsrokonofficial@gmail.com>
2025-08-20 19:56:12 +0600

    fetched just shops

--- src/pages/Shops.jsx
+++ src/pages/Shops.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../hooks/useAxiosSecure";
 
 
 const Shops = () => {
 
+    const axiosSecure = useAxiosSecure();
+    const [ shops, setShops ] = useState([]);
+    
+    useEffect(()=> {
+        axiosSecure.get('/shops')
+        .then(res => setShops(res.data))
+    },[])
+
+    console.log(shops);
     
 
     return (
         <div className="min-h-screen">
-            
+            <h1>total shops {shops.length}</h1>
         </div>
     );
 };
