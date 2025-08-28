3920db2ff1926aadbf4d27680a7aef61ec7bd0b6
hsrokon <hsrokonofficial@gmail.com>
2025-08-28 22:45:35 +0600

    shop details page made responsive

--- src/components/shops/ShopDetails.jsx
+++ src/components/shops/ShopDetails.jsx
@@ -30,29 +30,29 @@ const ShopDetails = () => {
     const enlistedDate = new Date(shop.enlistedIn).toLocaleDateString();
     
   return (
-    <div className="w-8/12 mx-auto mb-20">
+    <div className="w-11/12 lg:w-10/12 xl:w-8/12  mx-auto mb-20">
       {/* Image */}
       <div className="">
         <figure>
           <img
             src={shop.photoURL}
             alt={shop.placeName}
-            className="w-full h-[33rem] object-cover"
+            className="w-full h-40 md:h-[22rem] lg:h-[25rem] xl:h-[33rem] object-cover"
           />
         </figure>
         <div className="">
           {/*name */}
-          <div className="flex items-center justify-between bg-secondary/80 my-6 py-2 pl-4">
-            <h1 className="text-3xl font-bold text-white">{shop.placeName}</h1>
+          <div className="flex items-center justify-between bg-secondary/80 my-2 lg:my-4 xl:my-6 py-0.5 md:py-1 lg:py-2 pl-4">
+            <h1 className="text-lg lg:text-3xl font-bold text-white">{shop.placeName}</h1>
             {shop.halalCertified ? (
               <CheckCircle className="text-success" size={24} title="Halal Certified" />
             ) : ''}
           </div>
 
-          <div className="flex justify-between items-center">
+          <div className="flex flex-col text-sm md:flex-row md:justify-between md:items-center">
               {/* location */}
             <div className="flex items-center gap-2 text-gray-700">
-              <MapPin className="text-primary" size={26} />
+              <MapPin className="text-primary size-5 md:size-6 lg:size-8" />
               <div className="flex flex-col ">
                 <span>{shop.placeSpecificLocation}</span>
                 <span className="mr-auto text-accent">{shop.city}, {shop.country}</span>
@@ -65,8 +65,8 @@ const ShopDetails = () => {
             {/* tags */}
             <div className="flex flex-wrap gap-2">
               {shop.selectedTags.map((tag, i) => (
-                <span key={i} className="badge badge-outline flex text-base items-center gap-1">
-                  <Tag className="text-primary" size={20} /> {tag}
+                <span key={i} className="badge badge-outline flex text-sm items-center gap-1">
+                  <Tag className="text-primary size-3 md:size-4 lg:size-5 xl:size-6" /> {tag}
                 </span>
               ))}
           </div>
@@ -75,11 +75,11 @@ const ShopDetails = () => {
           </div>
 
           {/* review count and enlisted */}
-          <div className="flex justify-between items-center mt-4">
+          <div className="flex flex-col md:flex-row md:justify-between md:items-center mt-4">
             <p className="text-sm text-base-content hover:underline">
               <span className="font-semibold">Shop Added by : </span> 
-              {user && user.displayName}</p>
-            <p className="text-sm text-gray-400 hover:text-base-content hover:underline">Enlisted on: <span> </span> {enlistedDate}</p>
+              <span className="text-accent font-semibold">{user && user.displayName}</span></p>
+            <p className="text-xs md:tex-sm text-gray-500 hover:text-base-content hover:underline">Enlisted on : <span> </span> {enlistedDate}</p>
           </div>
         </div>
 
