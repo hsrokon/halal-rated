cc75d3320fe17f877d0805dabad478756228520e
hsrokon <hsrokonofficial@gmail.com>
2025-09-24 13:00:17 +0600

    just made a comeback

--- src/components/shopDetails/ShopDetailsRevCard.jsx
+++ src/components/shopDetails/ShopDetailsRevCard.jsx
@@ -34,12 +34,12 @@ const ShopDetailsRevCard = ({ id }) => {
     }
   }, [reviews]);
 
-  if (loading) return <p>Loading.......</p>;
+  if (loading) return <p>Loading.....</p>;
 
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-0.5 md:gap-2 lg:gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-0.5 md:gap-2   lg:gap-4">
         {reviews.length ? (
           reviews.map(review => (
             <ReviewCard 
